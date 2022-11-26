d020a36cc7d55083ce852be564d57fdcd9cc338b
ukhang dylan <ukhangmarma459@gmail.com>
2022-11-26 14:50:22 +0600

    footer-edit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     const date = new Date();
 
     return (
-        <footer className="py-4">
+        <footer className="py-4 mt-auto">
             <p className="text-center text-gray-400"> &copy; copyright {date.getFullYear()} number convetor (Ukhang)</p>
         </footer>
     )
